613a101d2de000542f274ffce07a2c6886c8df68
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-02-21 01:38:00 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
 
     # Wait for user input
     command = input()
+    if command == "exit 0":
+    sys.exit(0)
     print(f"{command}: command not found")
-    main()
 
 if __name__ == "__main__":
     main()
