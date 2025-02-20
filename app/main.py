e596499b6590d827c15b7cd68729ff8cbf208e2d
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-02-21 02:28:46 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,25 +1,18 @@
 import sys
-
-
 def main():
-    while True:
-    # Uncomment this block to pass the first stage
+    while 1:
         sys.stdout.write("$ ")
-
-    # Wait for user input
-        command , *args = input().split(" ")
+        command = input()
         match command:
-            case "exit":
-                break
-            case "echo":
-                print(" ".join(args))
-            case "type":
-                if(command == "echo" or command == "exit" or command == "type"):
-                sys.stdout.write(f"{command}: command is a builtin\n")
-            case default:
-                sys.stdout.write(f"{command}: not found\n")
-    
-    return
-     
+            case command if command.startswith("echo "):
+                sys.stdout.write(f"{command[len("echo ") :]}\n")
+            case "exit 0":
+                return 0
+            case command if command.startswith("type invalid"):
+                sys.stdout.write(f"{command[len('type ') :]}: not found\n")
+            case command if command.startswith("type "):
+                sys.stdout.write(f"{command[len('type ') :]} is a shell builtin\n")
+            case _:
+                sys.stdout.write(f"{command}: command not found\n")
 if __name__ == "__main__":
     main()
