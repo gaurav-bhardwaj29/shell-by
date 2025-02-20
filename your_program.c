59baa5c74ea91bc7ab0385f90e160bc6a2dc3045
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-02-21 01:24:55 +0530

    [any message]

--- your_program.c
+++ your_program.c
@@ -13,7 +13,12 @@ int main()
         printf("$ ");
         fflush(stdout);
         if (fgets(buffer, buffer_size, stdin) == NULL)
-        break;
+        {
+            printf("\n");
+            break;
+        }
+        if (sscanf(buffer, "%99s", command_name)==0)
+            continue;
         if (sscanf(buffer, "%99s", command_name)==1)
             printf("%s: command not found\n", command_name);
     }
