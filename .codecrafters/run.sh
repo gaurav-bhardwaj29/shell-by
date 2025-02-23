30e1bee23ca0cef30fb2111821b0a48ad2b2a18e
Gaurav Bhardwaj <98477295+gaurav-bhardwaj29@users.noreply.github.com>
2025-02-23 17:45:41 +0530

    Update run.sh to include C implementation

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -9,4 +9,9 @@
 # Exit early if any commands fail
 set -e
 
+# Compile the C program
+# gcc -o your_program your_program.c -lreadline
+
+# Execute the compiled program
+# exec ./your_program "$@"
 exec pipenv run python3 -u -m app.main "$@"
